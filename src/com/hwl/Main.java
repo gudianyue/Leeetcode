6baf4797b8e918da262845e68755a3aa77195b55
huowanliang <453203490@qq.com>
2022-06-13 22:11:07 +0800

    hot-fix commit

--- src/com/hwl/Main.java
+++ src/com/hwl/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 8, 3, 88, 2, 9, 6};
         System.out.println("hello, git");
+        System.out.println("hot-fix");
         quickSortByLoop(arr);
         for (int num : arr) {
             System.out.println(num);
